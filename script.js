4cf2f20d5feac361ab6cfb017758dc1fc7fac32b
Mharsh09 <mandlikharsh2904@gmail.com>
2025-06-17 07:09:37 +0530

    loginpage
    day2

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 // Select the button element
 const buttons = document.querySelectorAll('.button');
 
@@ -14,3 +12,26 @@ buttons.forEach((btn) => {
         btn.style.backgroundColor = '#06b488';
     });
 });
+
+const loginBtn = document.querySelector('.login_button');
+const modal = document.getElementById('login-modal');
+
+loginBtn.addEventListener('click', () => {
+  modal.style.display = 'flex';
+  document.body.classList.add('modal-open');
+});
+
+modal.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+    document.body.classList.remove('modal-open');
+  }
+});
+
+const closeBtn = document.querySelector('.close-modal');
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+  document.body.classList.remove('modal-open');
+});
+
