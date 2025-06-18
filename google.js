99bdad0ed957a7b89ce6216b1acddf6de9a48284
Mharsh09 <mandlikharsh2904@gmail.com>
2025-06-18 21:22:06 +0530

    google auth done
    editied verifypage and added google auth through firebase

---
+++ google.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { getAuth,GoogleAuthProvider,signInWithPopup} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCQYhjDaYLpbAjf8Irz5Q-IOmZy6p1IRw0",
+    authDomain: "growwclone-914ed.firebaseapp.com",
+    projectId: "growwclone-914ed",
+    storageBucket: "growwclone-914ed.firebasestorage.app",
+    messagingSenderId: "92561946534",
+    appId: "1:92561946534:web:7e8ba7cae6b007dde38167"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+auth.languageCode = 'en'; 
+
+const provider = new GoogleAuthProvider();
+
+const googleLogin = document.getElementById('google_login'); 
+
+googleLogin.addEventListener('click', function () {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const user = result.user;
+      console.log(user);
+      window.location.href = "../logged.html";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+});
--- script.js
+++ script.js
@@ -52,73 +52,27 @@ setInterval(() => {
 const conBtn = document.getElementById('subbtn');
 
 conBtn.addEventListener('click', () => {
-const buttons = document.querySelectorAll('.button');
+  const emailInput = document.getElementById('email-field');
+  const email = emailInput.value.trim();
+  const emailPattern = /^[A-Za-z\._\-0-9]+@[A-Za-z]+\.[a-z]{2,4}$/;
+  const errorMessage = document.querySelector('.error-message');
+  const line = document.querySelector('.wrap-input-2 .focus-border');
 
-buttons.forEach((btn) => {
-    btn.addEventListener('mouseover', () => {
-        btn.style.backgroundColor = '#04a77c';
-    });
-    btn.addEventListener('mouseout', () => {
-        btn.style.backgroundColor = '#06b488';
-    });
-});
+  if (!emailPattern.test(email)) {
+    errorMessage.style.display = 'block';
+    line.style.bottom = "15px";
+  } else {
+    const modalcontent = document.querySelector('.modal-rightcontent');
+    const verifyPage = document.querySelector('.verifypage');
+    const username =document.querySelector('.username');
+    const usermail=document.querySelector('.usermail').value;
 
-const getstartBtn = document.querySelector('.get-started');
-const loginBtn = document.querySelector('.login_button');
-const modal = document.getElementById('login-modal');
-
-getstartBtn.addEventListener('click', () => {
-  modal.style.display = 'flex';
-  document.body.classList.add('modal-open');
-});
-
-loginBtn.addEventListener('click', () => {
-  modal.style.display = 'flex';
-  document.body.classList.add('modal-open');
-});
-
-modal.addEventListener('click', (e) => {
-  if (e.target === modal) {
-    modal.style.display = 'none';
-    document.body.classList.remove('modal-open');
+    setTimeout(() => {
+      modalcontent.classList.add('fade-out');
+      modalcontent.style.display = 'none';
+      verifyPage.style.display = 'flex';
+      verifyPage.classList.add('fade-in');
+      username.textContent = `${usermail}`;
+    }, 500);
   }
 });
-
-const closeBtn = document.querySelector('.close-modal');
-
-closeBtn.addEventListener('click', () => {
-  modal.style.display = 'none';
-  document.body.classList.remove('modal-open');
-});
-
-const rotatingText = document.querySelector('.rotating-text');
-const words = ['Stocks', 'Mutual Funds', 'Fixed Deposits'];
-let index = 0;
-
-setInterval(() => {
-  rotatingText.classList.remove('fade-in');//removes
-  void rotatingText.offsetWidth; // reflow
-  index = (index + 1) % words.length;
-  rotatingText.textContent = words[index];
-  rotatingText.classList.add('fade-in');//adds
-}, 3500);
-
-const conBtn = document.getElementById('subbtn');
-
-conBtn.addEventListener('click', () => {
-  const righttopcontent = document.querySelector('.modal-right-top');
-  const rightbottomcontent = document.querySelector('.modal-right-bottom');
-  const rightfoot = document.querySelector('.foot');
-  const verifyPage = document.querySelector('.verifypage');
-
-
-  setTimeout(() => {
-    righttopcontent.classList.add('fade-out');
-    righttopcontent.style.display = 'none';
-    rightfoot.style.display = 'none';
-    verifyPage.style.display = 'flex';
-    verifyPage.classList.add('fade-in');
-  }, 500);
-});
-});
-
